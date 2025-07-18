bf099916c08b8657b3eeb4451de66a26107ceaf9
jose0305rm <81779667+jose0305rm@users.noreply.github.com>
2025-07-17 18:25:31 -0600

    connexion exitosa a la base de datos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,26 @@
-import { useEffect, useState } from 'react';
-import Empleados from './components/Empleados';
-import type { Empleado } from './types';
+import { useEffect, useState } from "react";
+import Empleados from "./components/Empleados";
+import type { Empleado } from "./types";
 
 function App() {
   const [empleados, setEmpleados] = useState<Empleado[]>([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/')
-      .then(res => res.json())
-      .then(data => setEmpleados(data.message));
+    fetch("http://127.0.0.1:8000/")
+      .then((res) => res.json())
+      .then((data) => setEmpleados(data.message));
   }, []);
 
   return (
-    <div>
-      <h1>Lista de empleados:</h1>
-      <ul>
-        {empleados.map((empleado) => (
-          <Empleados key={empleado.id} datos={empleado} />
-        ))}
-      </ul>
+    <div className="bg-gradient-to-t from-purple-900 to-purple-500 h-auto min-h-screen w-full mx-auto flex flex-col xl:flex-row items-center justify-center">
+      <div className="bg-white border rounded-lg shadow-lg p-6 w-full max-w-3xl">
+        <h1>Lista de empleados</h1>
+        <div>
+          {empleados.map((empleado) => (
+            <Empleados key={empleado.id} datos={empleado} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
